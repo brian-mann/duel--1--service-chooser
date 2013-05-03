a183cc4f7a0cb51499dbdbad5a2bea9bd6e2e2a6
Brian Mann <brian@osysit.com>
2013-05-03 14:02:35 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServiceChooser::Application.config.secret_token = 'edb8611fba2f2e1fbc50c0ea580ae1c4a4acfa2916f406dc613bef6b3c2b6f1d03e7dedf593e09a77378bd1f06ba9b0b752b645044e163c7f215aed09771751d'
